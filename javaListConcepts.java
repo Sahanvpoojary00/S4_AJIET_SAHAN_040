e064663d6afbb1a673d285121e48417e2a2d5113
#COMPILER <138592249+Sahanvpoojary00@users.noreply.github.com>
2026-05-09 08:54:50 +0000

    v1

--- javaListConcepts.java
+++ javaListConcepts.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 class javaListConcepts{
-    
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Sumith");
+        list.add("Manvith");
+        list.add("Pavan");
+        list.addFirst("Nithesh");
+        list.addLast("Sarthak");
+        System.out.println(list);
+        String s=list.get(0);
+        System.out.println(s);
+        System.out.println(list.remove(2));
+        System.out.println(list);
+
+    }
 }
